49ce5bfe4d2df674da4b41b11c9d59f23bb3b7b5
unknown <hectorlaucher6@gmail.com>
2026-03-25 20:27:21 -0300

    Contador e botão de alimentar funcionando porém há problemas com a imgem de fundo

--- atividadeDacio23_03_2026/app.js
+++ atividadeDacio23_03_2026/app.js
@@ -1,12 +1,13 @@
 "use strict"
-const img = document.getElementById("main");
+const cria = document.getElementById("main");
 const btn = document.getElementById("btn");
 
 const estados = {
     normal: "img/BichinSemFundoQA.png",
     comendo:"img/bichinComendoSemFundoQA.png",
     feliz:"img/BichinSemFundoQA.png",
-    fome: "img/BichinBravoQA.png"
+    fome: "img/BichinBravoQA.png",
+    morto:"img/BichinMortoQA.png"
 };
 
 let contador = 0;
@@ -14,18 +15,58 @@ let intervalo = null;
 let timeClick = null;
 let timeOut = null;
 
-function initCont(){
-    if(intervalo) clearInterval(intervalo);
-    intervalo = setInterval(() => { 
-    contador++;
+function controlador (){
+    if(intervalo) clearInterval(intervalo)
+        
+        intervalo = setInterval(() => {
+            contador++;
 
-    console.log("Tempo", contador);
+            console.log("tempo:",contador);
+            
+            if (contador == 30){
+                cria.src = estados.fome;
+            }
 
-    if(contador == 30){
-        img.src = estados.fome30
+            if(contador == 60){
+                cria.src = estados.morto;
+            }
+        }, 1000);
+}
+
+// function initCont(){
+//     if(intervalo) clearInterval(intervalo)
+
+//     intervalo = setInterval(() => { 
+//     contador++;
+
+//     console.log("Tempo", contador);
+
+//     if(contador == 30){
+//         cria.src = estados.fome;
+//     }
+//     if(contador == 60){
+//         cria.src = estados.morto;
+//     }
+
+//     }, 1000);
+// }
+
+
+    function alimentar(){
+        cria.src = estados.comendo;
+        contador = 0;
+        console.log("Comendo ");
+
+        if(timeClick) clearInterval(timeClick)
+
+            timeClick = setTimeout(() =>{
+                cria.src = estados.normal;
+                timeOut = setTimeout(() => {
+                    cria.src = estados.normal;
+                },2000);
+            },1000);
+        
+
     }
-    if(contador == 60){
-        img.src = estados.fome60
-    }
 
-    }, 1000)};
+    controlador();
